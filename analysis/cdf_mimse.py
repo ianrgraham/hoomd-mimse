ef251bc1dc4add01fe23d28fde50fe3812002c4d
Conor-Luppnow <conorluppnow@gmail.com>
2024-07-02 12:02:47 -0400

    seed may be acting off

--- analysis/cdf_mimse.py
+++ analysis/cdf_mimse.py
@@ -7,6 +7,7 @@ import matplotlib
 import pandas as pd
 import h5py
 import re
+import shutil
 
 from os.path import join, isfile
 import os
@@ -201,3 +202,50 @@ output_path = f'{workspace_PATH}/output/'
 
 postfix_arr = ['A','B','C','D','E','F','G']
 
+N = 256 ## number of particles
+DIM = 3 ## dimension of the system
+for k in range(len(postfix_arr)):
+    quen_states_path = f'{output_path}/quen_states_all_{postfix_arr[k]}'
+    pkl_state_file = f'quen_states.pkl'
+
+    with open(join(quen_states_path,pkl_state_file), 'rb') as fi:
+        globals()[f'mimse_state_{postfix_arr[k]}'] = pickle.load(fi)
+
+    globals()[f'distances_arr_{postfix_arr[k]}'] = []
+
+    globals()[f'mimse_state_{postfix_arr[k]}'] = np.array(globals()[f'mimse_state_{postfix_arr[k]}'])
+
+    globals()[f'count_{postfix_arr[k]}'] = 0
+    for i in range(len(globals()[f'mimse_state_{postfix_arr[k]}'])):
+        for j in range(len(globals()[f'mimse_state_{postfix_arr[k]}'])):
+            if (i != j):
+                dist = np.linalg.norm(globals()[f'mimse_state_{postfix_arr[k]}'][i]-globals()[f'mimse_state_{postfix_arr[k]}'][j])
+                globals()[f'distances_arr_{postfix_arr[k]}'].append(dist)
+        globals()[f'count_{postfix_arr[k]}'] += 1
+        print(f'prog_{postfix_arr[k]}',np.floor((globals()[f'count_{postfix_arr[k]}']/len(globals()[f'mimse_state_{postfix_arr[k]}']))*100))
+    globals()[f'distances_arr_{postfix_arr[k]}'] = np.array(globals()[f'distances_arr_{postfix_arr[k]}'])
+
+        # Calculate CDF
+    globals()[f'sorted_dist_{postfix_arr[k]}'] = np.sort(globals()[f'distances_arr_{postfix_arr[k]}'])
+    # print('sorted distances',sorted_distances_A)
+    globals()[f'cdf_{postfix_arr[k]}'] = np.arange(1, len(globals()[f'sorted_dist_{postfix_arr[k]}']) + 1) / len(globals()[f'sorted_dist_{postfix_arr[k]}'])
+
+    print('len cdf',len(globals()[f'cdf_{postfix_arr[k]}']))
+    print('len sorted distances',len(globals()[f'sorted_dist_{postfix_arr[k]}']))
+
+    # Plot CDF
+    plt.figure(0)
+
+    
+
+
+    plt.loglog(globals()[f'sorted_dist_{postfix_arr[k]}'], globals()[f'cdf_{postfix_arr[k]}'],label = f'{postfix_arr[k]}')
+    plt.xlim([4e0, 2e2])
+    plt.ylim([1e-2, 1.4])
+    plt.xlabel('Pairwise Euclidean Distance')
+    plt.ylabel('CDF')
+    plt.title(f'CDF')
+    plt.legend()
+
+plt.savefig(f'new_cdf.png')
+    
--- analysis/del_e_vs_del_r.py
+++ analysis/del_e_vs_del_r.py
@@ -27,7 +27,7 @@ import pickle
 from natsort import natsorted
 from sklearn.linear_model import LinearRegression
 
-
+'''
 file_path_en = f'/home/conor/Documents/Research/hoomd_mimse_Ian/hoomd-mimse/output/quen_states_all_A/quen_en.pkl'
 file_path_states = f'/home/conor/Documents/Research/hoomd_mimse_Ian/hoomd-mimse/output/quen_states_all_A/quen_states.pkl'
 # file_path_B = f'/home/conor/Documents/Research/hoomd_mimse_Ian/hoomd-mimse/output/quen_states_all_B/quen_B.pkl'
@@ -70,5 +70,66 @@ plt.plot(distances_A, mimse_en_A, 'o')
 plt.xlabel('Distance')
 plt.ylabel('Energy')
 plt.savefig('del_e_vs_del_r.png')
+'''
+
+output_path = f'{workspace_PATH}/output/'
+
+postfix_arr = ['A','B','C','D','E','F','G']
+
+N = 256 ## number of particles
+DIM = 3 ## dimension of the system
+for k in range(len(postfix_arr)):
+    # get path dirs for states and energies
+    quen_states_path = f'{output_path}/quen_states_all_{postfix_arr[k]}'
+    pkl_state_file = f'quen_states.pkl'
+
+    quen_en_path = f'{output_path}/quen_states_all_{postfix_arr[k]}'
+    pkl_en_file = f'quen_en.pkl'
+
+    # load using pickle
+    with open(join(quen_states_path,pkl_state_file), 'rb') as fi:
+        globals()[f'mimse_state_{postfix_arr[k]}'] = pickle.load(fi)
+
+    with open(join(quen_en_path,pkl_en_file), 'rb') as fi:
+        globals()[f'mimse_en_{postfix_arr[k]}'] = pickle.load(fi)
+
+    # set empty array for distances and energies
+    globals()[f'distances_arr_{postfix_arr[k]}'] = []
+    globals()[f'energies_arr_{postfix_arr[k]}'] = []
+
+
+    # make loaded data into numpy arrays
+    globals()[f'mimse_state_{postfix_arr[k]}'] = np.array(globals()[f'mimse_state_{postfix_arr[k]}'])
+    globals()[f'mimse_en_{postfix_arr[k]}'] = np.array(globals()[f'mimse_en_{postfix_arr[k]}'])
+
+    # set count to 0
+    globals()[f'count_{postfix_arr[k]}'] = 0
+
+    # set up empty array for positions | size: (length of states, N, DIM)
+    globals()[f'positions_big_{postfix_arr[k]}'] = np.zeros((len(globals()[f'mimse_state_{postfix_arr[k]}']), N, DIM))
+
+    # set reference state and energy(first state and energy)
+    ref_state = globals()[f'mimse_state_{postfix_arr[k]}'][0]
+    ref_en = globals()[f'mimse_en_{postfix_arr[k]}'][0]
+
+    for i in range(len(globals()[f'mimse_state_{postfix_arr[k]}'])):
+
+        # calculate distance and append to distances array
+        dist = np.linalg.norm(globals()[f'mimse_state_{postfix_arr[k]}'][i]-ref_state)
+        globals()[f'distances_arr_{postfix_arr[k]}'].append(dist)
+
+        globals()[f'count_{postfix_arr[k]}'] += 1
+        print(f'prog_{postfix_arr[k]}',np.floor((globals()[f'count_{postfix_arr[k]}']/len(globals()[f'mimse_state_{postfix_arr[k]}']))*100))
+
+    plt.figure(0)
+    plt.plot(globals()[f'distances_arr_{postfix_arr[k]}'], globals()[f'mimse_en_{postfix_arr[k]}'], 'o', label = f'{postfix_arr[k]}')
+    plt.xlabel('Distance')
+    plt.ylabel('Energy')
+    plt.legend()
+    
+
+plt.savefig(f'del_e_vs_del_r.png')
+    
+
 
 
